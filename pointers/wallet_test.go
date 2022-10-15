fd00ef7c3bfcb1ea51ea2079a0c6d1183beb4ad7
Darwin Perdomo <ZeroBl@hotmail.com>
2022-10-14 23:19:55 -0400

    feat: Add stringer interface to Bitcoin type

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,14 +3,14 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-  wallet := Wallet{}
+	wallet := Wallet{}
 
-  wallet.Deposit(Bitcoin(10))
+	wallet.Deposit(Bitcoin(10))
 
-  got := wallet.Balance()
-  want := Bitcoin(10)
+	got := wallet.Balance()
+	want := Bitcoin(10)
 
-  if got != want {
-    t.Errorf("got %d want %d", got, want)
-  }
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
 }
