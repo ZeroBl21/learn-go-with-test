51291d7996566456fb6ef82551cc88319e41891f
Darwin Perdomo <ZeroBl@hotmail.com>
2022-10-16 00:22:49 -0400

    refactor: Add now returns a error if word already exists

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -23,6 +23,31 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+
+  t.Run("new word", func(t *testing.T) {
+    dictionary := Dictionary{}
+    word := "test"
+    definition := "this is just a test"
+
+    dictionary.Add("test", "this is just a test")
+
+    assertDefinition(t, dictionary, word, definition)
+  })
+
+  t.Run("existing word", func(t *testing.T) {
+    word := "test"
+    definition := "this is just a test"
+
+    dictionary := Dictionary{word: definition}
+    err:= dictionary.Add(word, "new test")
+
+    assertError(t, err, ErrWordExists)
+    assertDefinition(t, dictionary, word, definition)
+  })
+}
+
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
@@ -37,16 +62,6 @@ func assertError(t testing.TB, got, want error) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
-}
-
-func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-
-	dictionary.Add("test", "this is just a test")
-
-	assertDefinition(t, dictionary, word, definition)
 }
 
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
