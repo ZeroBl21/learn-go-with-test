ba9e7447d9ec6df4e339c64f88168807179684ba
Darwin Perdomo <ZeroBl@hotmail.com>
2022-10-24 08:57:04 -0400

    feat(countdown.go): Add Sleeper type and methods

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -7,18 +7,33 @@ import (
 	"time"
 )
 
+// Sleeper allows you to put delays.
+type Sleeper interface {
+	Sleep()
+}
+
+// DefaultSleeper is an implementation of Sleeper with a predefined delay.
+type DefaultSleeper struct{}
+
+// Sleep will puase execution for the defined Duration.
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 const countdownStart = 3
 const finalWord = "Go!"
 
-func Countdown(out io.Writer) {
+// Countdown prints a countdown from 3 to out with a delay between count provided by Sleeper
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+    sleeper.Sleep()
 	}
 
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
