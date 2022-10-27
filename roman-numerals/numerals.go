3c202ba5cb811c6f44d3a2a741d1bbecc38ec976
Darwin Perdomo <ZeroBl@hotmail.com>
2022-10-27 09:17:12 -0400

    refactor(roman-numerals/): Change code for more reliability

--- roman-numerals/numerals.go
+++ roman-numerals/numerals.go
@@ -1,5 +1,29 @@
 package romannumerals
 
-func ConvertToRoman(num int) string {
-  return "I"
+import "strings"
+
+type RomanNumerals struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumerals{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
 }
