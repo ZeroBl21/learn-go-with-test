703eaa24049e6c3b06d669baed1ae43ebc781096
Darwin Perdomo <ZeroBl@hotmail.com>
2022-10-27 09:21:29 -0400

    feat(numerals.go): Update allRomanNumerals array with all numbers

--- roman-numerals/numerals.go
+++ roman-numerals/numerals.go
@@ -2,12 +2,20 @@ package romannumerals
 
 import "strings"
 
-type RomanNumerals struct {
+type RomanNumeral struct {
 	Value  int
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumerals{
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
 	{10, "X"},
 	{9, "IX"},
 	{5, "V"},
